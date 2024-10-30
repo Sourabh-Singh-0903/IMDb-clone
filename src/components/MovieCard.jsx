1258a05ca3e59bf83b60199c62b251392d352df3
Sourabh-Singh-0903 <114512043+Sourabh-Singh-0903@users.noreply.github.com>
2024-10-30 07:48:45 +0530

    Delete src directory

--- src/components/MovieCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function MovieCard() {
-  return (
-    <div className='h-[40vh] w-[180px] bg-center bg-cover rounded-xl hover:scale-110 duration-300 hover:cursor-pointer' style={{backgroundImage : `url(https://m.media-amazon.com/images/I/710cN59FCML._AC_UF894,1000_QL80_.jpg)`}}></div>
-  )
-}
-
-export default MovieCard
